d42401fe00cb4e4195662efcf7fef2b5f2c407bd
grahamdesmon <graham.desmon.simanjuntak@gmail.com>
2018-01-19 16:05:04 +0700

    add spoof utils

--- FaceRecognitionStart.py
+++ FaceRecognitionStart.py
@@ -6,6 +6,7 @@ face_model = './models/haarcascade_frontalface_default.xml'
 emotion_model = './models/emotion_1.hdf5'
 gender_model = './models/gender_1.hdf5'
 recognition_model = './models/recognition_1.h5'
+spoof_model = './models/spoof/modelCASIANB.pkl'
 gender_labels = {0: 'woman', 1: 'man'}
 verbose = 0
 # ---------------------------------------------------------
@@ -13,7 +14,7 @@ verbose = 0
 app = QtWidgets.QApplication(sys.argv)
 app.setWindowIcon(QtGui.QIcon('./images/badge.png'))
 w = ApplicationWindow(face_model, emotion_model, gender_model, recognition_model,
-                      gender_labels=gender_labels, verbose=verbose)
+                      spoof_model, gender_labels=gender_labels, verbose=verbose)
 w.setWindowTitle('Face Emotion and Gender Recognition')
 w.show()
 app.exec_()
